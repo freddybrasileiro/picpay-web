bcc31b0a03d55a78c71f60cb1260aaf39dc2028a
Freddy Brasileiro <freddybrasileiro@gmail.com>
2019-01-08 08:56:34 -0200

    loader

--- src/components/UsersList/Loader.js
+++ src/components/UsersList/Loader.js
@@ -1,5 +1,6 @@
 import React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const Loader = () => <div>Loader</div>;
+const Loader = () => <CircularProgress />;
 
 export default Loader;
